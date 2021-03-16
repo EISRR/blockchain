916a24ffb426ee75b48ad07d5fd432185952db01
EIS <evierstov@gmail.com>
2021-03-16 11:46:28 +0900

    update sol

--- first.sol
+++ first.sol
@@ -1,20 +1,22 @@
+pragma solidity >=0.4.22 <0.6.0;
 
 contract Test
 {
-    enum RequestType{NewHome, EditHome}
+    enum RequestType {NewHome, EditHome}
     
     struct Ownership
     {
         string homeAddress;
         address owner;
         uint p;
-    }   
+    }
     
-    struct Owner{
+    struct Owner
+    {
         string name;
         uint passSer;
         uint passNum;
-        uint256 date;
+        string date; //TODO переделать
         string phoneNumber;
     }
     
@@ -24,34 +26,73 @@ contract Test
         uint area;
         uint cost;
     }
+    
     struct Request
     {
         RequestType requestType;
         Home home;
         uint result;
-        address adr;
     }
+    
     struct Employee
     {
-        string nameEmployee;
+        string id;
+        string name;
         string position;
         string phoneNumber;
     }
     
-    mapping(address => Employee) private employees;
+    mapping(string => Employee) private employees;
     mapping(address => Owner) private owners;
     mapping(address => Request) private requests;
     mapping(string => Home) private homes;
     mapping(string => Ownership[]) private ownerships;
     
-    function AddHome(string memory _adr, uint _area, uint _cost) public {
+    // ДОМ
+    function AddHome(string memory _adr, uint _area, uint _cost) public
+    {
         Home memory h;
         h.homeAddress = _adr;
         h.area = _area;
         h.cost = _cost;
         homes[_adr] = h;
     }
-    function GetHome(string memory adr) public returns (uint _area, uint _cost){
+    
+    function GetHome(string memory adr) public returns(uint _area, uint _cost)
+    {
         return (homes[adr].area, homes[adr].cost);
+    }
+    
+    function EditHome(string memory _adr, uint _area, uint _cost) public
+    {
+        homes[_adr].area = _area;
+        homes[_adr].cost = _cost;
+    }
+    
+    // РАБОТНИК
+    function AddEmployee(string memory _id, string memory _name, string memory _position, string memory _phoneNumber) public
+    {
+        Employee memory e;
+        e.name = _name;
+        e.position = _position;
+        e.phoneNumber = _phoneNumber;
+        employees[_id] = e;
+    }
+    
+    function GetEmployee(string memory id) public returns(string memory _name, string memory _position, string memory _phoneNumber)
+    {
+        return (employees[id].name, employees[id].position, employees[id].phoneNumber);
+    }
+    
+    function EditEmployee(string memory _id, string memory _name, string memory _position, string memory _phoneNumber) public
+    {
+        employees[_id].name = _name;
+        employees[_id].position = _position;
+        employees[_id].phoneNumber = _phoneNumber;
+    }
+    
+    function DeleteEmployee(string memory id) public
+    {
+        delete employees[id];
     }
 }
